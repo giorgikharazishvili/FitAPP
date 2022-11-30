985be31d6badb98ad18d30e192f6a1f6f51f23cc
giorgikharazishvili <giorgi.kharazishvili@hotmail.com>
2022-11-30 14:46:16 +0400

    Initial Commit

---
+++ FitAPP/FitAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitAPPApp.swift
+//  FitAPP
+//
+//  Created by GIORGI's MacPro on 30.11.22.
+//
+
+import SwiftUI
+
+@main
+struct FitAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
